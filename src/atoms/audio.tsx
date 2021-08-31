36541e06a5712d8cd79850fdedb1c165ff232784
Robson Pinto <robson.pinto@aegea.com.br>
2021-08-30 21:12:30 -0300

    Initial commit

---
+++ src/atoms/audio.tsx
@@ -0,0 +1,11 @@
+export default function AtomsAudio() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
